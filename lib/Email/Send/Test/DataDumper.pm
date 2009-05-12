c5236308224d416a0be6f7e9147d333b4a1179e6
Nobuo Danjou <nobuo.danjou@gmail.com>
2009-05-12 13:57:31 +0900

    initial commit

---
+++ lib/Email/Send/Test/DataDumper.pm
@@ -0,0 +1,33 @@
+package Email::Send::Test::DataDumper;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Email::Send::Test::DataDumper -
+
+=head1 SYNOPSIS
+
+  use Email::Send::Test::DataDumper;
+
+=head1 DESCRIPTION
+
+Email::Send::Test::DataDumper is
+
+=head1 AUTHOR
+
+Nobuo Danjou E<lt>nobuo.danjou@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
